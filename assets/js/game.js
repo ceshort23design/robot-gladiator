3f881575452a0eb5f890765bd8349d26050b24e2
ceshort23 <ceshort23@gmail.com>
2021-02-02 18:10:59 -0600

    initial setup and practice

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!")
+// This creates a function called "fight"
+
+var playerName = window.prompt("What is your robots name?");
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+console.log(playerName);
+window.alert(playerName);
