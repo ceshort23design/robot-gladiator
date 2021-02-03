db20b897a72d4de1abe2506cc8a4d6d321b6efb9
ceshort23 <ceshort23@gmail.com>
2021-02-03 14:51:43 -0600

    create base sequence for the fight round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,35 @@
-// This creates a function called "fight"
 
+// player stats
 var playerName = window.prompt("What is your robots name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight () {
-    window.alert("The fight has begun!");
-}
+console.log(playerName, playerHealth, playerAttack);
 
-// fight();
-console.log(playerName);
-window.alert(playerName);
+// enemy stats
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of 'playerAttack' from 'enemyHealth' and use that result to update the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Subtract the value of 'enemyAttack' from 'playerHealth' and use that result to update the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+
+fight();
